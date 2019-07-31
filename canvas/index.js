d835213721f7a983ed42c1569b62e998fb455e06
Stephen Calnan <scalnan@trilogyed.com>
2019-07-31 13:05:43 -0700

    added basic index.js to transform markdown to html

---
+++ canvas/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+
+const showdown = require('showdown');
+showdown.setFlavor('github');
+
+const mdFilePath = '../01-html/content/step-1-html-cmd-git.md';
+
+async function transformMarkdown(path) {
+  return await readFile(path);
+}
+
+transformMarkdown(mdFilePath).then(data => {
+  const converter = new showdown.Converter();
+  console.log(converter.makeHtml(data));
+});
+
+
+
