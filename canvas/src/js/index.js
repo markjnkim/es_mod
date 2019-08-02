ac6d2dd3fd461c557a7fd0419e830a9b8df40184
Clark <cnielsen@trilogyed.com>
2019-08-02 11:38:31 -0700

    add webpack

---
+++ canvas/src/js/index.js
@@ -0,0 +1,34 @@
+// import other files into webpack compiler
+import prism from "prismjs";
+import styles from "../css/main.scss";
+
+$(document).ready(function() {
+  // find all learning blocks
+  $(".markdown-body blockquote").each((function() {
+    // get title element
+    var title = $(this).find("strong:first");
+    title.text(title.text().replace(":", ""));
+
+    // create easy name
+    var type = title.text().replace(/ /g, "-").toLowerCase();
+
+    // apply class to parent block
+    $(this).addClass(type);
+
+    // remove dangling span next to title
+    title.next("span").remove();
+
+    // move title outside of paragraph
+    title.prependTo($(this));
+
+    // add additional class for special types
+    if (type === "hint" || type === "pause") {
+      $(this).addClass("collapse");
+
+      // remove class once clicked
+      $(this).one("click", () => {
+        $(this).removeClass("collapse");
+      });
+    }
+  }));
+});
