e518131e92a244d856984380632f5aa2c89352c5
Clark <cnielsen@trilogyed.com>
2019-08-02 08:31:14 -0700

    block live files

--- canvas/build/extension-files/chrome.js
+++ canvas/build/extension-files/chrome.js
@@ -1,4 +1,14 @@
+// reload extension assets when icon is clicked
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.reload();
   chrome.runtime.reload();
 });
+
+// block currently live assets so local changes can override
+chrome.webRequest.onBeforeRequest.addListener(
+  function() { return {cancel: true}; },
+  {
+    urls: ["canvas-fsfo-theme.css", "canvas-fsfo-script.js"]
+  },
+  ["blocking"]
+);
