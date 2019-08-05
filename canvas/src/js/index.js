f21a170976e8ce40cd8dcfed99395f094a83cc7b
Clark <cnielsen@trilogyed.com>
2019-08-05 10:16:21 -0700

    extra precautions with wrapper, code blocks

--- canvas/src/js/index.js
+++ canvas/src/js/index.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
   // add custom wrapper if not already there
   if ($("#bootcamp").length == 0) {
     $('#wiki_page_show .user_content').wrapInner("<div id='bootcamp'>");
+    $("#bootcamp h1.page-title").prependTo($("#bootcamp").parent());
   }
 
   // find crappy tables
@@ -66,6 +67,12 @@ $(document).ready(function() {
   });
 
   // make sure code snippets are formatted for prism
-  $("pre:not(:has(>code))").wrapInner("<code class='language-bash'>");
+  $("pre:not(:has(>code))").each(function() {
+    // default to bash if no class is set
+    var className = $(this).attr("class").indexOf("language-") !== -1 ? $(this).attr("class") : "language-bash";
+
+    $(this).wrapInner(`<code class="${className}">`);
+  });
+
   Prism.highlightAll();
 });
