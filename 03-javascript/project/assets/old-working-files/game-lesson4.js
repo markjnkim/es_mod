dba0797db513c5aa66f3eb89bb7389b0160b6b6e
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2019-09-10 11:30:24 -0400

    Lesson 5 - fix

--- 03-javascript/project/assets/old-working-files/game-lesson4.js
+++ 03-javascript/project/assets/old-working-files/game-lesson4.js
@@ -177,7 +177,7 @@ var randomNumber = function(min, max) {
 
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?");,
+  name: window.prompt("What is your robot's name?"),
   health: 100,
   attack: 10,
   money: 10,
