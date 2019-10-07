1df880b9832dc7fd88b87dfdd2d6f5ce8ce1b142
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2019-10-07 11:56:17 -0400

    Lesson 1

---
+++ 04-Web-APIs/project/assets/js/script.js
@@ -0,0 +1,11 @@
+var buttonEl = document.getElementById("save-task");
+var tasksToDoEl = document.getElementById("tasks-to-do");
+
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
